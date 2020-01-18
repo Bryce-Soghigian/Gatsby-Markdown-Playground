3cc801a60f4d5ed6c12ccb2ef28eb9e1daf8a7aa
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-01-18 03:48:54 -0600

    markdown page working

--- markdown-playground/src/pages/index.js
+++ markdown-playground/src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react"
-
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby blog with Markdown pages.</p>
+    <Link to="/blog/my-first-post/">Go to my first Markdown blog post</Link>
   </Layout>
 )
 
